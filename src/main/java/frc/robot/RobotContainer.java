afe2cb90654747ca1265bba711d3db3f85ff278a
Me-Smrt <code_t@krummhere.com>
2024-08-20 20:47:17 -0400

    Added bubbler subsystem - TK

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,14 +5,18 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.arcadedrive;
 import frc.robot.subsystems.Controls;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Lidar;
 import frc.robot.subsystems.LightEmittingDiode;
 import frc.robot.subsystems.Pneumatics;
+import frc.robot.subsystems.taticalSpheroidGenerator;
 
 public class RobotContainer {
   public RobotContainer() {
@@ -25,11 +29,15 @@ public class RobotContainer {
   public static Controls controls = Controls.getInstance();
   public static XboxController m_controller = new XboxController(0);
   public static Lidar m_lidar = new Lidar();
+  public static taticalSpheroidGenerator bubble = taticalSpheroidGenerator.getInstance();
 
   private void configureBindings() {
     driveTrain.setDefaultCommand(new arcadedrive(driveTrain));
 
-    // new JoystickButton(RobotContainer.m_controller, Button.kLeftBumper.value).whileTrue(new InstantCommand(() -> led.setLedColorSolid(255, 0, 0)));
+    new JoystickButton(RobotContainer.m_controller, Button.kLeftBumper.value)
+        .onTrue(new InstantCommand(() -> bubble.on()));
+    new JoystickButton(RobotContainer.m_controller, Button.kLeftBumper.value)
+        .onFalse(new InstantCommand(() -> bubble.off()));
   }
 
   public static void controls() {
---
+++ src/main/java/frc/robot/subsystems/taticalSpheroidGenerator.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class taticalSpheroidGenerator extends SubsystemBase {
+  private static taticalSpheroidGenerator instance = null; 
+
+  private DigitalOutput toggle = new DigitalOutput(0);
+
+  public static taticalSpheroidGenerator getInstance() {
+    if (instance == null) {
+      instance = new taticalSpheroidGenerator();
+    }
+    return instance; 
+  } 
+
+  /** Creates a new taticalSphereoidGenerator. */
+  private taticalSpheroidGenerator() {
+    toggle.set(false);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void on() {
+    toggle.set(true);
+  }
+
+  public void off() {
+    toggle.set(false);
+  }
+}
